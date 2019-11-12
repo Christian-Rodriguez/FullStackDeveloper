0f6e6ea253955b696113c22911f1b59d59806c3b
sergio <sergiohidalgocaceres@gmail.com>
2019-11-11 21:31:03 -0500

    FormsModule (Parte 2)

--- Angular/Clase03/appFormsModule/src/app/app.component.ts
+++ Angular/Clase03/appFormsModule/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild("formulario", { static: false }) formRegistro: NgForm
+
   nombre: string = "Marcela"
 
-  ingresar(algo) {
-    console.log(algo)
+  cargarData() {
+    this.formRegistro.setValue({
+      nombre: "Alfonso",
+      correo: "elcomercio@correo.com",
+      contrasena: ""
+    })
+  }
+
+  cargarDataParcial() {
+    this.formRegistro.form.patchValue({
+      nombre: "Piero",
+      correo: "piero@correo.com"
+    })
+  }
+
+  resetear() {
+    this.formRegistro.reset({
+      nombre: "Javier"
+    })
+  }
+
+  ingresar() {
+    /*     if (this.formRegistro.valid) {
+          console.log("Formulario válido")
+        } */
+    // console.log(this.formRegistro)
+    console.log(this.formRegistro.value)
   }
 }
