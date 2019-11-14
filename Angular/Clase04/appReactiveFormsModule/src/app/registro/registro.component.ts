da78bf33a24da3d101b1c3c53673dc9da144e583
EduardoHoyos <edu.hoyos92@gmail.com>
2019-11-13 22:05:03 -0500

    Custom Validator

--- Angular/Clase04/appReactiveFormsModule/src/app/registro/registro.component.ts
+++ Angular/Clase04/appReactiveFormsModule/src/app/registro/registro.component.ts
@@ -10,13 +10,30 @@ export class RegistroComponent implements OnInit {
 
   grupo: FormGroup
 
+  dominiosGratuitos: Array<string> = ["gmail.com", "outlook.com", "hotmail.com", "yahoo.com"]
+
   constructor() {
     this.grupo = new FormGroup({
       nombre: new FormControl(null, Validators.required),
-      correo: new FormControl(null, [Validators.required, Validators.email]),
-      contrasena: new FormControl(null, Validators.required),
-      confirmacion: new FormControl(null, Validators.required)
+      correo: new FormControl(null, [Validators.required, Validators.email, this.validarCorreosGratuitos.bind(this)]),
+      contrasena: new FormControl(null, [Validators.required, Validators.pattern("[a-zA-Z]{3,5}[0-9]{4}")]),
+      confirmacion: new FormControl(null, Validators.required),
+      terminos: new FormControl(null, Validators.requiredTrue)
     })
+  }
+
+  validarCorreosGratuitos(fc: FormControl): { [s: string]: boolean } {
+    if (fc && fc.value && fc.value.trim() != "" && fc.value.split("@").length > 1) {
+      const dominio = fc.value.split("@")[1].toLowerCase()
+
+      if (this.dominiosGratuitos.indexOf(dominio) > -1) {
+        return { correoGratuito: true }
+      }
+
+      return null
+    }
+
+    return null
   }
 
   ngOnInit() {
