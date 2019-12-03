527c36e03ddded0ec07c1c7d8144619e8c3e92d6
sergio <sergiohidalgocaceres@gmail.com>
2019-12-02 22:17:28 -0500

    Observables - parte 2

--- Angular/Clase08/appRutas/src/app/login/login.component.ts
+++ Angular/Clase08/appRutas/src/app/login/login.component.ts
@@ -17,74 +17,24 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
 
-    const obs: Observable<string> = Observable.create(
-      (observador: Observer<string>) => {
-        setTimeout(() => {
-          observador.next("Tocaron la puerta")
-        }, 2000)
-
-        setTimeout(() => {
-          observador.next("Llegó el inspector de la empresa eléctrica")
-        }, 4000)
-
-
-        setTimeout(() => {
-          observador.next("Vinieron sus familiares")
-        }, 10000)
-
-        setTimeout(() => {
-          observador.error("Se incendió la casa")
-        }, 8000)
-
-        setTimeout(() => {
-          observador.complete()
-        }, 7000)
+    const obs: Observable<Array<{}>> = Observable.create(
+      (observador: Observer<Array<{}>>) => {
+        const http: XMLHttpRequest = new XMLHttpRequest()
+        http.onreadystatechange = function () {
+          if (this.status == 200 && this.readyState == 4) {
+            observador.next(JSON.parse(this.responseText))
+          }
+        }
+        http.open("get", "https://jsonplaceholder.typicode.com/todos")
+        http.send()
       }
     )
 
     this.suscripcion = obs.subscribe(
-      mensaje => console.log("Mensaje", mensaje),
+      mensaje => this.resultados = mensaje,
       error => console.log("Error", error),
       () => console.log("Complete", "Se terminó la chamba")
     )
-
-
-
-
-    /* const promesa: Promise<any> = new Promise((resolve, reject) => {
-      const http: XMLHttpRequest = new XMLHttpRequest()
-      http.onreadystatechange = function () {
-        if (this.status == 200 && this.readyState == 4) {
-          resolve(this.responseText)
-        }
-      }
-      http.open("get", "https://jsonplaceholder.typicode.com/todos")
-      http.send()
-    })
-
-    promesa.then(
-      (data) => { this.resultados = JSON.parse(data) },
-      (error) => { console.log(error) }
-    )
-
-    const promesaFotos: Promise<any> = new Promise((resolve, reject) => {
-      const http: XMLHttpRequest = new XMLHttpRequest()
-      http.onreadystatechange = function () {
-        if (this.status == 200 && this.readyState == 4) {
-          resolve(this.responseText)
-        }
-      }
-      http.open("get", "https://jsonplaceholder.typicode.com/photos")
-      http.send()
-    })
-
-    promesaFotos.then(
-      (data) => { this.resultadosFotos = JSON.parse(data) },
-      (error) => { console.log(error) }
-    ) */
-
-
-
   }
 
   ngOnDestroy() {
