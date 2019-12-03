353d1665f786caaba87ab5f3be606ed2c0082d3e
sergio <sergiohidalgocaceres@gmail.com>
2019-12-02 21:13:13 -0500

    Promesas

--- Angular/Clase08/appRutas/src/app/login/login.component.ts
+++ Angular/Clase08/appRutas/src/app/login/login.component.ts
@@ -1,34 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-	selector: 'app-login',
-	templateUrl: './login.component.html',
-	styleUrls: ['./login.component.css']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-	resultados = []
+  resultados = []
+  resultadosFotos = []
 
-	constructor() { }
+  constructor() { }
 
-	ngOnInit() {
-		const promesa: Promise<any> = new Promise((resolve, reject) => {
-			const http: XMLHttpRequest = new XMLHttpRequest()
-			http.onreadystatechange = function () {
-				if (this.status == 200 && this.readyState == 4) {
-					resolve(this.responseText)
-				}
-			}
-			http.open("get", "https://jsonplaceholder.typicode.com/todos")
-			http.send()
-		})
+  ngOnInit() {
+    const promesa: Promise<any> = new Promise((resolve, reject) => {
+      const http: XMLHttpRequest = new XMLHttpRequest()
+      http.onreadystatechange = function () {
+        if (this.status == 200 && this.readyState == 4) {
+          resolve(this.responseText)
+        }
+      }
+      http.open("get", "https://jsonplaceholder.typicode.com/todos")
+      http.send()
+    })
 
-		promesa.then(
-			(data) => { this.resultados = JSON.parse(data) },
-			(error) => { console.log(error) }
-		)
+    promesa.then(
+      (data) => { this.resultados = JSON.parse(data) },
+      (error) => { console.log(error) }
+    )
 
+    const promesaFotos: Promise<any> = new Promise((resolve, reject) => {
+      const http: XMLHttpRequest = new XMLHttpRequest()
+      http.onreadystatechange = function () {
+        if (this.status == 200 && this.readyState == 4) {
+          resolve(this.responseText)
+        }
+      }
+      http.open("get", "https://jsonplaceholder.typicode.com/photos")
+      http.send()
+    })
 
-	}
+    promesaFotos.then(
+      (data) => { this.resultadosFotos = JSON.parse(data) },
+      (error) => { console.log(error) }
+    )
+
+    /*  promesaFotos
+       .then(
+         (data) => { this.resultadosFotos = JSON.parse(data) }
+       )
+       .catch(
+         (error) => { console.log(error) })
+
+     promesaFotos
+       .then(
+         (data) => { this.resultadosFotos = JSON.parse(data) }
+       )
+
+     promesaFotos.catch(
+       (error) => { console.log(error) }) */
+
+  }
 
 }
