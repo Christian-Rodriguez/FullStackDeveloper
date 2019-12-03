9a0d56467b1782ec48c791e9595d1c36c4261625
sergio <sergiohidalgocaceres@gmail.com>
2019-12-02 21:54:57 -0500

    Observables (parte 1)

--- Angular/Clase08/appRutas/src/app/login/login.component.ts
+++ Angular/Clase08/appRutas/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Observer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -10,10 +11,47 @@ export class LoginComponent implements OnInit {
   resultados = []
   resultadosFotos = []
 
+  suscripcion: Subscription
+
   constructor() { }
 
   ngOnInit() {
-    const promesa: Promise<any> = new Promise((resolve, reject) => {
+
+    const obs: Observable<string> = Observable.create(
+      (observador: Observer<string>) => {
+        setTimeout(() => {
+          observador.next("Tocaron la puerta")
+        }, 2000)
+
+        setTimeout(() => {
+          observador.next("Llegó el inspector de la empresa eléctrica")
+        }, 4000)
+
+
+        setTimeout(() => {
+          observador.next("Vinieron sus familiares")
+        }, 10000)
+
+        setTimeout(() => {
+          observador.error("Se incendió la casa")
+        }, 8000)
+
+        setTimeout(() => {
+          observador.complete()
+        }, 7000)
+      }
+    )
+
+    this.suscripcion = obs.subscribe(
+      mensaje => console.log("Mensaje", mensaje),
+      error => console.log("Error", error),
+      () => console.log("Complete", "Se terminó la chamba")
+    )
+
+
+
+
+    /* const promesa: Promise<any> = new Promise((resolve, reject) => {
       const http: XMLHttpRequest = new XMLHttpRequest()
       http.onreadystatechange = function () {
         if (this.status == 200 && this.readyState == 4) {
@@ -43,23 +81,14 @@ export class LoginComponent implements OnInit {
     promesaFotos.then(
       (data) => { this.resultadosFotos = JSON.parse(data) },
       (error) => { console.log(error) }
-    )
+    ) */
 
-    /*  promesaFotos
-       .then(
-         (data) => { this.resultadosFotos = JSON.parse(data) }
-       )
-       .catch(
-         (error) => { console.log(error) })
 
-     promesaFotos
-       .then(
-         (data) => { this.resultadosFotos = JSON.parse(data) }
-       )
 
-     promesaFotos.catch(
-       (error) => { console.log(error) }) */
+  }
 
+  ngOnDestroy() {
+    this.suscripcion.unsubscribe()
   }
 
 }
